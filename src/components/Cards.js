d5861123b5eb120eb8236770e35332fc3d81deb6
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-07-17 15:39:47 -0400

    Added comments to make code easier to read

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,19 +4,21 @@ export default class Objects extends Component {
   render() {
     return (
       <div className="card">
+        {/* Title */}
         {this.props.data ? (
           <h1>{this.props.data[0].title}</h1>
         ) : (
           <h1>Loading...</h1>
         )}
+        {/* Youtube Video */}
         {this.props.data ? (
           <iframe
             title="video"
             src={this.props.data[0].url}
             width="560"
             height="315"
-            frameborder="0"
-            allowfullscreen
+            frameBorder="0"
+            allowFullScreen
           />
         ) : (
           <h1>Loading...</h1>
